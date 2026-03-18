5ce5aec04c5444a8918cbe46783bbffda036141a
nitesh.dev <nitesh.dev@proton.me>
2026-03-18 13:26:27 +0530

    feat: now date and time on the task view works

---
+++ assets/scripts/handle-time.js
@@ -0,0 +1,20 @@
+// in this file all function for handle time and data on the taskbar
+
+const placeholder = document.querySelector(".time-date");
+const timePlaceholder = placeholder.children[0];
+const timeFormat = {
+    hour: "2-digit",
+    minute: "2-digit",
+    hour12: true,
+};
+const datePlaceholder = placeholder.children[1];
+const dateFormat = {};
+
+setInterval(() => {
+    let date = new Date();
+    datePlaceholder.innerHTML = date.toLocaleDateString("en-IN", {
+        dateStyle:"medium"
+    });
+    console.log(datePlaceholder.innerHTML)
+    timePlaceholder.innerHTML = date.toLocaleTimeString("en-US", timeFormat);
+}, 1000);
